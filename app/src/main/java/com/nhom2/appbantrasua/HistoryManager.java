1849db6e3bf4d347542a10fe1b6a855aeffc98a3
Devcujx2112 <Vutungduongnight@gmail.com>
2024-10-01 21:36:09 +0700

    App dat tra sua

---
+++ app/src/main/java/com/nhom2/appbantrasua/HistoryManager.java
@@ -0,0 +1,30 @@
+package com.nhom2.appbantrasua;
+
+import com.nhom2.appbantrasua.Entity.History;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryManager {
+    private static HistoryManager instance;
+    private static List<History> ListHistory;
+
+    private HistoryManager() {
+        ListHistory = new ArrayList<>();
+    }
+
+    public static HistoryManager getInstance() {
+        if (instance == null) {
+            instance = new HistoryManager();
+        }
+        return instance;
+    }
+
+    public List<History> getListHistory() {
+        return ListHistory;
+    }
+
+    public void setListHistory(List<History> listHistory) {
+        ListHistory = listHistory;
+    }
+}
